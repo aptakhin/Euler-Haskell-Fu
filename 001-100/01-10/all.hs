60791d37daf5fbfada64f5c7e6f99153b793d30f
Alexander Ptakhin <mail@aptakhin.name>
2013-09-09 16:22:08 +0400

    Messed

--- 001-100/01-10/all.hs
+++ 001-100/01-10/all.hs
@@ -1,11 +1,1 @@
 let p001 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
-
-fibonacci :: Int -> Int -> Int  
-fibonacci a b = a + b
-fibonacci 1 1
-
-lucky :: (Integral a) => a -> String  
-lucky 7 = "LUCKY NUMBER SEVEN!"  
-lucky x = "Sorry, you're out of luck, pal!"   
-
-let p002 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
