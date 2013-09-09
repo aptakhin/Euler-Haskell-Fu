8b3ff7cfcb302d47b7721b3ac2414fc4552ad5b7
Alexander Ptakhin <mail@aptakhin.name>
2013-09-09 18:00:03 +0400

    Yes

---
+++ 001-100/01-10/001.hs
@@ -0,0 +1,7 @@
+import Data.List  
+import GHC.Exts
+import GHC.Integer
+import Prelude
+
+main = do
+	sum [1..999]
--- 001-100/01-10/all.hs
+++ 001-100/01-10/all.hs
@@ -1,1 +1,15 @@
-let p001 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+--
+-- 001 - Multiples of 3 and 5
+--
+p001 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+--
+-- 002 - Even Fibonacci numbers
+--
+fib 0 = 0
+fib 1 = 1
+fib n = fib(n - 1) + fib(n - 2)
+
+p002 = do
+	--let x = [1..4000000]
+	let f = sum [k = fib(x) | x <- [1..4000000], k `mod` 2 == 0]
